af02ba5f23ab7ff858875001b8c84bda49bc393b
Keerthivassan <keerthyevans@gmail.com>
2026-07-12 12:11:37 +0530

    added no. of (active/alive) workers field in status command

--- src/commands/status.js
+++ src/commands/status.js
@@ -1,16 +1,20 @@
 import { listJobs } from '../core/jobStore.js';
+import { getActiveWorkerPids } from '../core/workerManager.js';
 
 export function registerStatusCommand(program) {
   program
     .command('status')
-    .description('Show summary of all job states')
+    .description('Show summary of all job states & active workers')
     .action(async () => {
       const jobs = await listJobs();
-      
+
         let stat = jobs.reduce((jobStat , job) => {
             jobStat[job.state]++;
-            return jobStat;                
+            return jobStat;
         }, { pending : 0 , processing : 0 , completed : 0 , failed : 0 , dead : 0 , total : jobs.length});
+
+        const activeWorkers = await getActiveWorkerPids();
+        stat.active_workers = activeWorkers.length;
 
         console.log(stat);
 
